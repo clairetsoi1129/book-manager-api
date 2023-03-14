bfe35a80a1c7b7e4d70d98e6f2ec732e11068487
Claire Tsoi <clairetsoi1129@gmail.com>
2023-03-14 13:32:09 +0000

    Add - different application properties for different env  using environment variable

--- src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
+++ src/main/java/com/techreturners/bookmanager/BookmanagerApplication.java
@@ -1,5 +1,6 @@
 package com.techreturners.bookmanager;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookmanagerApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
 		SpringApplication.run(BookmanagerApplication.class, args);
 	}
 
